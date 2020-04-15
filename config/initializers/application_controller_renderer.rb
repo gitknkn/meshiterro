b876a4dfe43fa454fd04460285be314901e4edff
gitknkn <atio.asu@gmail.com>
2020-04-15 23:20:37 +0900

    [add] 6-5

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'afab4738b11d105d9f24d06320f68cff87bb738dcf1e1e72dce8ffc48ea254698fcb3d5311983a423b513c7af75d3876188807960b9c57de5796bfa83b26ef8e'
